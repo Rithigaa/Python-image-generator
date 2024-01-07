087b7ef7606bcf3a7968698ed315e4fddb4ef755
Rithigaa <108213520+Rithigaa@users.noreply.github.com>
2024-01-07 14:58:46 +0530

    Add files via upload

---
+++ img generator.py
@@ -0,0 +1,18 @@
+import os
+import urllib.request
+from datetime import datetime
+import openai
+
+openai.api_key=os.getenv("OPENAI_API_KEY")
+user_prompt=input("Write your prompt ")
+response=openai.Image.create(
+    prompt=user_prompt,
+    n=1,
+    size="1024x1024"
+)
+
+image_url=response['data'][0]['url']
+print(image_url)
+
+file_name="image"+datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+".png"
+urllib.request.urlretrieve(image_url,file_name)
